23eb0951e9fa6e2d0dd48a5de4da697e89e9eb79
Sirius Bell <vlad10game@gmail.com>
2023-07-21 23:48:29 +0300

    Some stupid annotations

--- weathercast/config.py
+++ weathercast/config.py
@@ -1,20 +1,33 @@
 import os
 import sys
 
+from asynctinydb import TinyDB
 from dotenv import load_dotenv
 from loguru import logger
+from vkbottle import API
+from vkbottle.bot import BotLabeler
 
 load_dotenv()
 
-VK_TOKEN = os.getenv('VK_TOKEN')
-VK_GROUP_ID = os.getenv('VK_GROUP_ID')
-WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+VK_TOKEN: str = os.getenv('VK_TOKEN')
+VK_GROUP_ID: int = int(os.getenv('VK_GROUP_ID'))
+WEATHER_API_KEY: str = os.getenv('WEATHER_API_KEY')
 
-LEVEL = "DEBUG"
+# logger level
+LEVEL: str = "DEBUG"
 
+# database name
+DB_NAME: str = "db.json"
+
+# logging options
 logger.remove()
 logger.add(os.path.join("logs", "log.log"), format="[{time:YYYY-MM-DD at HH:mm:ss}] [{level}]: {message}",
            level=LEVEL, retention="10 days")
 logger.add(sys.stderr,
            format="<green>[{time:YYYY-MM-DD at HH:mm:ss}]</green> <cyan>[{level}]</cyan>: <level>{message}</level>",
            level=LEVEL, colorize=True)
+
+db: TinyDB = TinyDB(os.path.join(os.getcwd(), DB_NAME))
+
+api: API = API(token=VK_TOKEN)
+labeler: BotLabeler = BotLabeler()
