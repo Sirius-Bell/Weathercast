8f05fc7615008a3b0a75d30e69f49148ff1fc609
Sirius <vlad10game@gmail.com>
2023-08-22 22:04:30 +0300

    Add new parameter: city, on default "Астрахань"

--- weathercast/middlewares/db_mid.py
+++ weathercast/middlewares/db_mid.py
@@ -16,4 +16,4 @@ class DatabaseCheck(BaseMiddleware[Message]):
     async def pre(self):
         if not await db.search(where('peer_id') == self.event.peer_id):
             await db.insert({'id': self.event.id, 'peer_id': self.event.peer_id, 'name': self.event.get_user(),
-                             'allow_sending': True, })
+                             'allow_sending': True, "city": "Астрахань"})
