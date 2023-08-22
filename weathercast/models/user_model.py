ad67d08018febbee6006a1ddaf5e79720d90f6df
Sirius <vlad10game@gmail.com>
2023-08-22 22:05:23 +0300

    Add new parameter: city, on default "Астрахань"

--- weathercast/models/user_model.py
+++ weathercast/models/user_model.py
@@ -10,10 +10,11 @@ from dataclasses import dataclass
 @dataclass
 class User:
     """
-    User model.
+    User model for database(without using this dataclass)
     Represents user in id, peer_id, name, allow_sending
     """
     id: int
     peer_id: int
     name: str
     allow_sending: bool = True
+    city: str = "Астрахань"
