8d8335a477c6e6929d06fe241602da96a397e903
Sirius <vlad10game@gmail.com>
2023-08-23 21:18:24 +0300

    Cosmetic changes

--- weathercast/middlewares/db_mid.py
+++ weathercast/middlewares/db_mid.py
@@ -9,11 +9,13 @@ from vkbottle import BaseMiddleware
 from vkbottle.bot import Message
 
 from weathercast.config import db
+from asynctinydb import Query
+from loguru import logger
 
 
 class DatabaseCheck(BaseMiddleware[Message]):
 
     async def pre(self):
-        if not await db.search(where('peer_id') == self.event.peer_id):
-            await db.insert({'id': self.event.id, 'peer_id': self.event.peer_id, 'name': self.event.get_user(),
+        if not await db.search(Query().peer_id == self.event.peer_id):
+            await db.insert({'id': self.event.id, 'peer_id': self.event.peer_id,
                              'allow_sending': True, "city": "Астрахань"})
